b1d1ff66eb0d721dd0dbe6a6784f4995d6cfe56a
nikhilhonkalaskar-coder <nikhilhonkalaskar@gmail.com>
2025-12-27 12:35:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import "dotenv/config";
+
 import express from "express";
 import cors from "cors";
 import axios from "axios";
@@ -82,4 +82,5 @@ app.post("/api/verify-otp", (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
 });
+
 
