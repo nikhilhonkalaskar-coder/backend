4807c2389b3716615ed0eb599162d8fca9c436e2
nikhilhonkalaskar-coder <nikhilhonkalaskar@gmail.com>
2026-01-06 11:13:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -185,3 +183,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
